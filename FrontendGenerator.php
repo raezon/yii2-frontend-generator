c0d8cf5af50456b2b567b8f0108de813080752ba
ammar <amardjebabla10@gmail.com>
2024-10-23 17:53:01 +0200

    Fixing ui

--- FrontendGenerator.php
+++ FrontendGenerator.php
@@ -69,6 +69,6 @@ class FrontendGenerator extends Generator
 
     public function getTemplatePath()
     {
-        return \Yii::getAlias('@app/modules/ui/generator/default/' . $this->templateName);
+        return \Yii::getAlias('@app/modules/ui/generator/' . $this->templateName);
     }
 }
