5de11a38a6ea5251af7357c7e47261d381d60154
ammar <amardjebabla10@gmail.com>
2024-10-23 21:31:50 +0200

    Creating a basic generator to create vue, react, angular app from yii gii generator

--- form.php
+++ form.php
@@ -10,36 +10,25 @@ use yii\widgets\ActiveForm;
 ?>
 
 <div class="frontend-generator-form">
-
     <?php $form = ActiveForm::begin(); ?>
 
-    <!-- View Name Input -->
     <?= $form->field($generator, 'viewName')->textInput(['maxlength' => true]) ?>
 
-    <!-- Project Path Input (Custom Path for file generation) -->
     <?= $form->field($generator, 'projectPath')->textInput(['maxlength' => true])->label('Path of Project') ?>
 
-    <!-- Framework Selection Dropdown -->
     <?= $form->field($generator, 'framework')->dropDownList([
         'vue' => 'Vue.js',
         'react' => 'React.js',
         'angular' => 'Angular',
     ], ['prompt' => 'Select Framework']) ?>
 
-    <!-- Enable I18N -->
     <?= $form->field($generator, 'enableI18N')->checkbox() ?>
 
-    <!-- Message Category -->
     <?= $form->field($generator, 'messageCategory')->textInput(['maxlength' => true]) ?>
-
-    <!-- Code Template -->
-    <?= $form->field($generator, 'templateName')->dropDownList($generator->templates, ['prompt' => 'Select Code Template']) ?>
-
 
     <div class="form-group">
         <?= Html::submitButton('Generate', ['class' => 'btn btn-primary']) ?>
     </div>
 
     <?php ActiveForm::end(); ?>
-
 </div>
