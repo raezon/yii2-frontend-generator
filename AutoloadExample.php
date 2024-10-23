9b715cb8df85d2571a271c6b92baa47ecfafd6c8
ammar <amardjebabla10@gmail.com>
2024-10-23 17:10:23 +0200

    Intialisation

--- AutoloadExample.php
+++ AutoloadExample.php
@@ -2,13 +2,60 @@
 
 namespace app\modules\ui\generator;
 
-/**
- * This is just an example.
- */
-class AutoloadExample extends \yii\base\Widget
+use yii\gii\Generator;
+use yii\helpers\Html;
+
+class FrontendGenerator extends Generator
 {
-    public function run()
+    public $viewName = 'hello-world';
+
+    public function getName()
     {
-        return "Hello!";
+        return 'Frontend Hello World Generator';
+    }
+
+    public function getDescription()
+    {
+        return 'This generator creates a simple Hello World view.';
+    }
+
+    public function rules()
+    {
+        return array_merge(parent::rules(), [
+            ['viewName', 'filter', 'filter' => 'trim'],
+            ['viewName', 'required'],
+            ['viewName', 'match', 'pattern' => '/^\w+(?:-\w+)*$/', 'message' => 'Only word characters and dashes are allowed.'],
+        ]);
+    }
+
+    public function attributeLabels()
+    {
+        return array_merge(parent::attributeLabels(), [
+            'viewName' => 'View Name',
+        ]);
+    }
+
+    public function hints()
+    {
+        return array_merge(parent::hints(), [
+            'viewName' => 'This is the name of the view file to be generated.',
+        ]);
+    }
+
+    public function generate()
+    {
+        $files = [];
+        $viewPath = \Yii::$app->getViewPath();
+        $templatePath = $this->getTemplatePath() . '/view.php';
+        $files[] = new \yii\gii\CodeFile(
+            $viewPath . '/' . $this->viewName . '.php',
+            $this->render($templatePath)
+        );
+        return $files;
+    }
+
+    public function getTemplatePath()
+    {
+        return \Yii::getAlias('@app/modules/ui/generator/default');
     }
 }
